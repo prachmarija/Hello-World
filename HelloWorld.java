87375012e416cfb493106fb6926c11a39536a7d9
Mariia Dobberstein <Mariia_Dobberstein@epam.com>
2017-02-07 12:45:42 +0100

    Hello World program first edition

---
+++ HelloWorld.java
@@ -0,0 +1,11 @@
+
+
+/**
+ * Created by Mariia_Dobberstein on 2/6/2017.
+ */
+public class HelloWorld {
+    public static void main(String[] args)
+    {
+        System.out.print("Hello World!");
+    }
+}
